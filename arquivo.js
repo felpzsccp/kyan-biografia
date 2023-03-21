dc2ae0224a6a8f26d9dc4769485d182c10789204
Felipe Stamboni <123281130+felpzsccp@users.noreply.github.com>
2023-03-21 18:50:04 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,32 @@
+// passo 1 - dar um jeito de pegar o elemento HTML dos botões
+
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens =document.querySelectorAll('.imagem');
+
+//passo 2 - dar um jeito de identificar o clique do usuário no botão
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        console.log(indice);
+
+        //passo 3 - desmarcar o botão selecionado anterior
+        const botaoSelecionado = document.querySelector('.selecionado');
+        botaoSelecionado.classList.remove('selecionado');
+
+        //passo 4 - marcar o botao clicado como se estivesse selecionado
+        botao.classList.add('selecionado');
+
+        // passo 5 - esconder a imagem de fundo anterior
+
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+
+        //passo 6 - fazer aparecer a imagem de fundo correspondente ao botao clicado
+
+        console.log(imagens);
+        imagens[indice].classList.add('ativa');
+
+    })
+
+})
